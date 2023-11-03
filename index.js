2f0cbebcd5de74ff750a127aa55b5187e6e942d6
Nehal Bobhate <142580510+NehalBobhate@users.noreply.github.com>
2023-11-03 23:45:04 +0530

    Added Extra Features
    Added extra math function like sin, cos, tan, log & Squareroot.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-function removeLast(op) {
-				let c=op.split('');
-				c.pop();
-				let ans=c.join('');
-				return ans;
-			}
+function removeLast(op)
+{
+  let c=op.split('');
+  c.pop();
+  let ans=c.join('');
+  return ans;
+}
+function giveSin(op) {
+	
+ let radians = op * Math.PI/180;
+ op=Math.sin(radians).toFixed(3);
+  return op;
+
+}
+function giveCos(op) {
+	
+ let radians = op * Math.PI/180;
+ op=Math.cos(radians).toFixed(3);
+ return op;
+
+}
+function giveTan(op) {
+	
+ let radians = op * Math.PI/180;
+ op=Math.tan(radians).toFixed(3);
+ return op;
+
+}
